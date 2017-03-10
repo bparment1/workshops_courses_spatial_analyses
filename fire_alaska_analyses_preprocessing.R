8d59457637969ac2de061d5d098dfd42fb42e4bc
Benoit Parmentier <benoit.parmentier@gmail.com>
2017-03-10 10:45:00 -0500

    masking data and transforming to tiff

--- fire_alaska_analyses_preprocessing.R
+++ fire_alaska_analyses_preprocessing.R
@@ -5,12 +5,12 @@
 #
 #AUTHORS: Benoit Parmentier                                             
 #DATE CREATED: 03/09/2017 
-#DATE MODIFIED: 03/09/2017
+#DATE MODIFIED: 03/10/2017
 #Version: 2
 #PROJECT: AAG 2017 workshop preparation
 #TO DO:
 #
-#COMMIT: initial commit
+#COMMIT: masking data and transforming to tiff
 #
 #################################################################################################
 
@@ -36,12 +36,9 @@ library(bitops)
 
 ###### Functions used in this script
 
-#function_spatial_regression_analyses <- "SPatial_analysis_spatial_reg_03082017_functions.R" #PARAM 1
-#function_paper_figures_analyses <- "space_beats_time_sbt_paper_figures_functions_01092016.R" #PARAM 1
-#script_path <- "/home/parmentier/Data/Space_beats_time/sbt_scripts" #path to script #PARAM 2
-#script_path <- "/home/bparmentier/Google Drive/Space_beats_time/sbt_scripts"
-#source(file.path(script_path,function_spatial_regression_analyses)) #source all functions used in this script 1.
-#source(file.path(script_path,function_paper_figures_analyses)) #source all functions used in this script 1.
+function_preprocessing_and_analyses <- "fire_alaska_analyses_preprocessing_functions_03102017.R" #PARAM 1
+script_path <- "/home/bparmentier/Google Drive/Data/Seminars_talks_workshops/workshops/AAG2017_spatial_temporal_analysis_R/R_scripts"
+source(file.path(script_path,function_preprocessing_and_analyses)) #source all functions used in this script 1.
 
 #####  Parameters and argument set up ###########
 
@@ -49,14 +46,14 @@ in_dir_NDVI <- "/home/bparmentier/Google Drive/Data/Seminars_talks_workshops/wor
 
 out_dir <- "/home/bparmentier/Google Drive/Data/Seminars_talks_workshops/workshops/AAG2017_spatial_temporal_analysis_R/Fire_Alaska_analyses/outputs"
 
-#moore_window <- file.path(in_dir,"window_test4.rst")
-#winds_zones_fname <- file.path(in_dir,"00_windzones_moore_sin.rst")
+mask_alaska_filename <- "/home/bparmentier/Google Drive/Data/Seminars_talks_workshops/workshops/AAG2017_spatial_temporal_analysis_R/Fire_Alaska_analyses/Lab_time_series/Data_unprocessed/mask_Alaska_11112014.rst" 
 
 proj_modis_str <-"+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs" #CONST 1
 #CRS_interp <-"+proj=longlat +ellps=WGS84 +datum=WGS84 +towgs84=0,0,0" #Station coords WGS84
 CRS_WGS84 <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +towgs84=0,0,0" #Station coords WGS84 # CONST 2
 proj_str<- CRS_WGS84 
 CRS_reg <- CRS_WGS84 # PARAM 4
+
 
 file_format <- ".rst" #PARAM5
 NA_value <- -9999 #PARAM6
@@ -81,8 +78,14 @@ if(create_out_dir_param==TRUE){
   setwd(out_dir) #use previoulsy defined directory
 }
 
+
+r_mask <- raster(mask_alaska_filename)
 lf <- mixedsort(list.files(path=in_dir_NDVI,pattern="NDVI_2001_2009_filling6__NDVI_gap_filling2_07062011_test1_.*.rst$",full.names=T))
 
 r_NDVI_data <- stack(lf)
+
+plot(r_NDVI_data,y=1)
+
+r_NDVI_data_mask <- mask(r_NDVI_data,mask=r_mask,filename=file.path(out_dir,"r_NDVI_data_alaska_mask.tif"))
 
 ################### End of Script #########################
