dcd4c4b00421464dc88b28fa210d81801a59abca
Benoit Parmentier <benoit.parmentier@gmail.com>
2017-03-13 10:45:00 -0400

    masking and compressing NDVI time series in tif and comparison of sizes

--- fire_alaska_analyses_preprocessing.R
+++ fire_alaska_analyses_preprocessing.R
@@ -5,7 +5,7 @@
 #
 #AUTHORS: Benoit Parmentier                                             
 #DATE CREATED: 03/09/2017 
-#DATE MODIFIED: 03/11/2017
+#DATE MODIFIED: 03/13/2017
 #Version: 2
 #PROJECT: AAG 2017 workshop preparation
 #TO DO:
@@ -16,23 +16,23 @@
 
 ###Loading R library and packages                                                      
 
-library(sp)
-library(rgdal)
-library(spdep)
-library(gtools)
-library(maptools)
-library(parallel)
-library(rasterVis)
-library(raster)
+library(sp) # spatial/geographfic objects and functions
+library(rgdal) #GDAL/OGR binding for R with functionalities
+library(spdep) #spatial analyses operations, functions etc.
+library(gtools) # contains mixsort and other useful functions
+library(maptools) # 
+library(parallel) # parallel computation, part of base package no
+library(rasterVis) # raster visualization operations
+library(raster) # raster functionalities
 library(forecast) #ARIMA forecasting
-library(xts)
-library(zoo)
-library(lubridate)
+library(xts) #extension for time series object and analyses
+library(zoo) # time series object and analysis
+library(lubridate) # dates functionality
 library(colorRamps) #contains matlab.like color palette
-library(rgeos)
-library(sphet) #contains spreg
+library(rgeos) #contains topological operations
+library(sphet) #contains spreg, spatial regression modeling
 library(BMS) #contains hex2bin and bin2hex
-library(bitops)
+library(bitops) #
 
 ###### Functions used in this script
 
@@ -54,7 +54,7 @@ CRS_WGS84 <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +towgs84=0,0,0" #Station c
 proj_str<- CRS_WGS84 
 CRS_reg <- CRS_WGS84 # PARAM 4
 
-
+masking_process <- F
 file_format <- ".rst" #PARAM5
 NA_value <- -9999 #PARAM6
 NA_flag_val <- NA_value #PARAM7
@@ -82,6 +82,7 @@ if(create_out_dir_param==TRUE){
 r_mask <- raster(mask_alaska_filename)
 NAvalue(r_mask) <- -999
 plot(r_mask)
+freq(r_mask)# Alaska mask count of pixels
 
 lf <- mixedsort(list.files(path=in_dir_NDVI,pattern="NDVI_2001_2009_filling6__NDVI_gap_filling2_07062011_test1_.*.rst$",full.names=T))
 
@@ -89,9 +90,57 @@ r_NDVI_data <- stack(lf)
 
 plot(r_NDVI_data,y=1)
 
-r_NDVI_data_mask <- mask(r_NDVI_data,mask=r_mask,filename=file.path(out_dir,"r_NDVI_data_alaska_mask2.tif"))
+if(masking_process==T){
+  r_NDVI_data_mask <- mask(r_NDVI_data,mask=r_mask,filename=file.path(out_dir,"r_NDVI_data_alaska_mask3.tif"))
+}else{
+  r_NDVI_data_mask <- brick(file.path(out_dir,"r_NDVI_data_alaska_mask3.tif"))
+}
 
+##
 plot(r_NDVI_data_mask,y=2)
 filename(r_NDVI_data_mask) 
 
+date_range <- c("2001.01.01","2009.12.31") #NDVI Katrina
+
+#generate dates for 16 days product
+dates_val <- generate_dates_by_step(date_range[1],date_range[2],16)$dates #NDVI Katrina
+dates_val_str <- format(dates_val,format="%Y_%m_%d") #format to insert in names of raste files, convert "-" in "_"
+
+out_suffix_str <- paste0("NDVI_",dates_val_str) # this needs to be matching the number of outputs files writeRaste
+
+#note idrisi files are 19 mb compared to 6.3 after compression in the tif format
+writeRaster(r_NDVI_data_mask,filename="alaska.tif",
+            bylayer=T,datatype="FLT4S",options="COMPRESS=LZW",suffix=out_suffix_str,overwrite=T)
+
+lf_masked <- mixedsort(list.files(path= out_dir, pattern="^alaska_NDVI_.*.tif",full.names=T))
+
+#Comparing size of files after conversion, masking and compression in tif
+file.info(lf_masked[1])$size/(1024*1024) # this is in bytes, convert to mb
+file.info(lf[1])$size/(1024*1024)
+
+file.info(lf_masked[1])$size/(1024*1024)/(file.info(lf[1])$size/(1024*1024)) # this is about 35.8% the size of the original raster file 
+
+dim(r_NDVI_data_mask)
+
+##### Now convert other files from *.rst to *.tif
+
+#~/Google Drive/Data/Seminars_talks_workshops/workshops/AAG2017_spatial_temporal_analysis_R/Fire_Alaska_analyses/Lab_time_series/Data_unprocessed
+lf_var <- list.files(path="~/Google Drive/Data/Seminars_talks_workshops/workshops/AAG2017_spatial_temporal_analysis_R/Fire_Alaska_analyses/Lab_time_series/Data_unprocessed",pattern="*.rst$",full.names=T)
+
+r_var <- stack(lf_var)
+dim(r_var)
+plot(r_var,1)
+
+## now mask and transform one by one...
+r_var_data_mask <- mask(r_var,mask=r_mask,filename=file.path(out_dir,"r_var_masked.tif"))
+out_suffix_str <- names(r_var_data_mask) # this needs to be matching the number of outputs files writeRaste
+#Fix here problem.
+#note idrisi files are 19 mb compared to 6.3 after compression in the tif format
+writeRaster(r_var_data_mask,filename="r.tif",
+            bylayer=T,datatype="FLT4S",options="COMPRESS=LZW",suffix=out_suffix_str,overwrite=T)
+
+
 ################### End of Script #########################
+
+#To explore later:
+#http://stackoverflow.com/questions/12670972/doing-pca-on-very-large-data-set-in-r
