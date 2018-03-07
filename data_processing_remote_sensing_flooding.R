c7bde2f1b136b867ef98060de6bbc6b102c7b847
Benoit Parmentier <bparmentier@sshgw02.research.sesync.org>
2018-03-07 10:45:00 -0500

    testing aggregation for NLCD data

--- data_processing_remote_sensing_flooding.R
+++ data_processing_remote_sensing_flooding.R
@@ -101,6 +101,322 @@ generate_dates_by_step <-function(start_date,end_date,step_date){
   return(dates_obj)
 }
 
+aggregate_raster_fun <- function(l_rast,zonal_colnames,use_majority,agg_fact,agg_fun,file_format,rast_ref,num_cores,out_suffix, out_dir){
+  #
+  #Function to aggregate input raster stack
+  #if use majority then the zonal layer is aggregated and then reclassfied based by the majority rule
+  
+  lf_agg <- mclapply(l_rast,
+                     FUN=aggregate_raster,
+                     #r_in=raster(lf_layerized_bool[1]),
+                     agg_fact=agg_fact,
+                     reg_ref_rast=NULL,
+                     #agg_fun="mean",
+                     agg_fun=agg_fun,
+                     out_suffix=NULL,
+                     file_format=file_format,
+                     out_dir=out_dir,
+                     out_rast_name = NULL,
+                     mc.preschedule=FALSE,
+                     mc.cores = num_cores) 
+  
+  l_rast_original <- l_rast
+  l_rast <- unlist(lf_agg) 
+  
+  ###Break out and get mean per class and do majority rule!
+  
+  if(use_majority==TRUE){
+    
+    #l_rast_original
+    #r_r_srtm_Katrina_rec2_NDVI_Katrina_03162017.rst"
+    #r <- raster(paste0("r_",zonal_colnames,"_",out_suffix,file_format,sep=""))
+    raster_name <- (paste0("r_",zonal_colnames,"_",out_suffix,file_format,sep=""))
+    out_suffix_str <- paste0("agg5_zonal","_",out_suffix)
+    #debug(generate_soft_cat_aggregated_raster_fun)
+    lf_agg_soft <- generate_soft_cat_aggregated_raster_fun(raster_name,
+                                                           reg_ref_rast=NULL,
+                                                           agg_fact,
+                                                           agg_fun,
+                                                           num_cores,
+                                                           NA_flag_val=NA_flag_val,
+                                                           file_format,
+                                                           out_dir,
+                                                           out_suffix_str)
+    
+    reclass_val <- unique(raster(raster_name)) #unique zonal values to reassign
+    #reclass_val <- c(0,1,2) # value for the elevation reclassified
+    
+    #function_spatial_regression_analyses <- "SPatial_analysis_spatial_reg_functions_04072017b.R" #PARAM 1
+    #script_path <- "/home/bparmentier/Google Drive/Space_beats_time/sbt_scripts"
+    #source(file.path(script_path,function_spatial_regression_analyses)) #source all functions used in this script 1.
+    #debug(reclass_in_majority)
+    
+    r_reclass_obj <- reclass_in_majority(r_stack=stack(lf_agg_soft),
+                                         threshold_val=NULL,
+                                         max_aggregation = TRUE,
+                                         reclass_val = reclass_val)
+    
+    plot(r_reclass_obj$r_rec)
+    rast_zonal <- r_reclass_obj$r_rec
+    #zonal_colnames
+    raster_name <- paste0("agg_",agg_fact,"_","r_",zonal_colnames,"_",out_suffix,file_format)
+    
+    writeRaster(rast_zonal,
+                filename=file.path(out_dir,raster_name),
+                overwrite=TRUE)  
+    
+  }
+  
+  if(use_majority==FALSE){
+    #sure SRTM and reclass based on threshold values?
+    
+  }
+  
+  #r_srtm_Katrina_rec2
+  #-rw-rw-r-- 1 bparmentier bparmentier 1894 Apr  7 12:33 r_r_srtm_Katrina_rec2_NDVI_Katrina_04062017.tif
+  #-rw-rw-r-- 1 bparmentier bparmentier 1016 Apr  7 12:34 agg_5_r_r_srtm_Katrina_rec2_NDVI_Katrina_04062017.tif
+  
+  ###
+  zonal_colnames <- gsub(extension(raster_name),"",raster_name)
+  ##
+  
+  ##########################
+  #### prepare return object
+  
+  obj <- list(zonal_colnames,l_rast,l_rast_original)
+  names(obj) <- c("zonal_colnames","l_rast","l_rast_original")
+  
+  return(obj)
+}
+
+reclass_in_majority <- function(r_stack,threshold_val=0.5,max_aggregation=FALSE,reclass_val){
+  ##
+  #This function reclassify a set of soft layers using the majority or maximum value rule.
+  #When max_aggregation is TRUE, the max value rule is used in the aggregation.
+  #
+  #INPUTS
+  #1) r_stack
+  #2) threshold_val
+  #3) max_aggregation
+  #4) reclass_val
+  #
+  
+  ## Reclass
+  if(!is.null(threshold_val) & (max_aggregation==FALSE)){
+    r_rec_threshold <- r_stack > threshold_val
+    #use calc later to save directly the file
+    #
+    
+    r_rec_val_s <- lapply(1:nlayers(r_rec_threshold),
+                          function(i,r_stack){df_subs <- data.frame(id=c(0,1),v=c(0,reclass_val[i]));
+                          x <- subs(subset(r_stack,i), df_subs)},r_stack=r_rec_threshold)
+    r_rec_val_s <- stack(r_rec_val_s) #this contains pixel above 0.5 with re-assigned values
+    r_rec <- calc(r_test,function(x){sum(x)})
+    
+    ### prepare return object
+    reclass_obj <- list(r_rec,r_rec_val_s)
+    names(reclass_obj) <- c("r_rec","r_rec_val_s")
+    
+  }
+  
+  if(max_aggregation==TRUE){
+    #r_zonal_agg_soft <- stack(lf_agg_soft)
+    #Find the max, in stack of pixels (can be used for maximum compositing)
+    r_max_s <- calc(r_stack, function(x) max(x, na.rm = TRUE))
+    #maxStack <- stackApply(r_zonal_agg_soft, indices=rep(1,nlayers(r_zonal_agg_soft)), fun = max, na.rm=TRUE)
+    r_max_rec_s <- overlay(r_stack,r_max_s, fun=function(x,y){as.numeric(x==y)})
+    r_ties <- sum(r_max_rec_s) #find out ties
+    #this may be long
+    #freq_r_rec_df <- freq(r_rec_max,merge=T)
+    
+    r_rec_val_s <- lapply(1:nlayers(r_max_rec_s),
+                          function(i,r_stack){df_subs <- data.frame(id=c(0,1),v=c(0,reclass_val[i]));
+                          x <- subs(subset(r_stack,i), df_subs)},r_stack=r_max_rec_s)
+    r_rec_val_s <- stack(r_rec_val_s)
+    r_rec <- calc(r_rec_val_s,function(x){sum(x)})#overlays the layer with sum, 
+    #x2 <- subs(r, df, subsWithNA=FALSE)
+    
+    ### prepare return object
+    reclass_obj <- list(r_rec,r_rec_val_s,r_max_rec_s,r_ties)
+    names(reclass_obj) <- c("r_rec","r_rec_val_s","r_max_rec_s","r_ties")
+  }
+  
+  ###
+  return(reclass_obj)
+}
+
+generate_soft_cat_aggregated_raster_fun <- function(r,reg_ref_rast,agg_fact,agg_fun,num_cores,NA_flag_val,file_format,out_dir,out_suffix){
+  ## Function to aggregate categories
+  
+  ##INPUTS
+  #1) r: raster to aggregate and crop/project if necessary
+  #2) reg_ref_rast: reference raster, it must have a coordinate system defined
+  #3) agg_fact:
+  #4) agg_fun:
+  #5) num_cores: number of cores used in the parallel processsing
+  #6) NA_flag_val: flag value used for no data
+  #7) file_format: raster file format e.g. .tif, .rst
+  #8) out_dir: output directory
+  #9) out_suffix: output suffix added to files
+  #
+  #OUTPUTS
+  #
+  #
+  #
+  
+  
+  ##### STEP 1: Check input
+  
+  if(class(r)!="RasterLayer"){
+    r <- raster(r)
+  }
+  
+  NAvalue(r) <- NA_flag_val #make sure we have a flag value assigned
+  
+  ###### STEP 1: BREAK OUT
+  ## Breakout layers: separate categories in individual layers
+  
+  freq_tb <- as.data.frame(freq(r)) #zero is NA?
+  ## get the names
+  names_val <- freq_tb$value
+  names_val <- names_val[!is.na(names_val)] #remove NA
+  
+  ## Make a brick composed of multiple layers: one layer per category (in one unique multiband file)
+  out_raster_name <- file.path(out_dir,paste("r_layerized_bool_",out_suffix,file_format,sep=""))
+  r_layerized <- layerize(r,
+                          classes=names_val,
+                          filename= out_raster_name,
+                          overwrite=T)
+  list_out_raster_name <- file.path(out_dir,paste("r_layerized_bool_",names_val,"_",out_suffix,file_format,sep=""))
+  
+  ## Now write out separate layers in one unique file (one per categories)
+  #notice some issue here, looping through
+  #for(i in 1:nlayers){
+  #  writeRaster(r_layerized,
+  #              bylayer=T,
+  #              suffix=paste0(names_val,"_",out_suffix),
+  #              filename=paste("r_layerized_bool",
+  #                             file_format,sep="")
+  #             ,overwrite=T)
+  #}
+  
+  writeRaster(r_layerized,
+              bylayer=T,
+              suffix=paste0(names_val,"_",out_suffix),
+              filename=paste("r_layerized_bool",
+                             file_format,sep="")
+              ,overwrite=T)
+  
+  lf_layerized_bool <- paste("r_layerized_bool_",names_val,"_",out_suffix,file_format,sep="")
+  #names(r_layerized) <- 
+  #inMemory(r_date_layerized)
+  #filename(r_date_layerized)
+  #r_test <- raster(lf_layerized_bool[1])
+  
+  ### STEP 2: aggregate
+  ## Aggregate
+  
+  ##To do
+  ##Set correct names based on categories of input
+  ##Set output name
+  
+  #r_agg_fname <-aggregate_raster(agg_fact=agg_fact,
+  #                               r_in=r_date_layerized,reg_ref_rast=NULL,agg_fun="mean",out_suffix=NULL,file_format=".tif",out_dir=NULL)
+  #debug(aggregate_raster)
+  #r_agg_fname <-aggregate_raster(r_in=raster(lf_layerized_bool[1]),
+  #                               agg_fact=agg_fact,
+  #                               reg_ref_rast=NULL,
+  #                               #agg_fun="mean",
+  #                               agg_fun=agg_fun,
+  #                               out_suffix=NULL,
+  #                               file_format=file_format,
+  #                               out_dir=out_dir,
+  #                               out_rast_name = NULL)
+  #r_var_s <- mclapply(1:length(infile_var),FUN=import_list_modis_layers_fun,list_param=list_param_import_modis,mc.preschedule=FALSE,mc.cores = num_cores) #This is the end bracket from mclapply(...) statement
+  
+  lf_agg <- mclapply(lf_layerized_bool,
+                     FUN=aggregate_raster,
+                     #r_in=raster(lf_layerized_bool[1]),
+                     agg_fact=agg_fact,
+                     reg_ref_rast=NULL,
+                     #agg_fun="mean",
+                     agg_fun=agg_fun,
+                     out_suffix=NULL,
+                     file_format=file_format,
+                     out_dir=out_dir,
+                     out_rast_name = NULL,
+                     mc.preschedule=FALSE,
+                     mc.cores = num_cores) 
+  #r_agg <- stack(unlist(lf_agg))
+  raster_outname <- unlist(lf_agg)
+  #apply function by layer using lapply.
+  
+  ## Reclassify by labels
+  return(raster_outname)
+}
+
+
+
+#Function to aggregate from fine to coarse resolution, this will change accordingly once the input raster ref is given..
+#
+aggregate_raster <- function(r_in, agg_fact, reg_ref_rast=NULL,agg_fun="mean",out_suffix=NULL,file_format=".tif",out_dir=NULL,out_rast_name=NULL){
+  #Aggregate raster from raster input and reference file
+  #INPUT arguments:
+  #1) r_in: input raster layer
+  #2) agg_fact: factor to aggregate
+  #3) reg_ref_rast: reference raster to match in resolution, if NULL then send a message
+  #4) agg_fun: default is mean
+  #5) out_suffix: output suffix
+  #6) file_Format: raster format used e.g. .tif
+  #7) out_dir: output directory
+  #8) out_rast_name: output raster name if null it is created from the input file name
+  #OUTPUT:
+  # out_raster_name: name of the file containing the aggregated raster
+  #
+  # Authors: Benoit Parmentier
+  # Created: 10/15/2015
+  # Modified: 05/10/2016
+  # To Do: 
+  # - Add option to disaggregate
+  #
+  ################################
+  
+  ##If file is provided rather than RasterLayer
+  if(class(r_in)!="RasterLayer"){
+    r_in <- raster(r_in)
+  }
+  
+  if(is.null(agg_fact)){
+    res_ref <- res(reg_ref_rast)[1] #assumes square cells, and decimal degrees from WGS84 for now...
+    res_in <- res(r_in)[1] #input resolution, assumes decimal degrees
+    agg_fact <-round(res_ref/res_in) #find the factor needed..
+    #fix this to add other otpions e.g. aggregating down
+  }
+  
+  #Default values...
+  if(is.null(out_suffix)){
+    out_suffix <- ""
+  }
+  
+  if(is.null(out_dir)){
+    out_dir <- "."
+  }
+  
+  ## Create output raster name if out_rast_name is null
+  if(is.null(out_rast_name)){
+    raster_name <- filename(r_in)
+    extension_str <- extension(raster_name)
+    raster_name_tmp <- gsub(extension_str,"",basename(raster_name))
+    out_rast_name <- file.path(out_dir,paste("agg_",agg_fact,"_",raster_name_tmp,out_suffix,file_format,sep="")) #output name for aster file
+    #out_rast_name <- raster_name #for use in function later...
+  }
+  
+  r_agg <- aggregate(r_in, fact=agg_fact,FUN=agg_fun,filename=out_rast_name,overwrite=TRUE)
+  
+  return(out_rast_name)
+  
+}
+
 #function_preprocessing_and_analyses <- "fire_alaska_analyses_preprocessing_functions_03102017.R" #PARAM 1
 #function_analyses <- "exercise2_fire_alaska_analyses_functions_03232017.R" #PARAM 1
 script_path <- "/nfs/bparmentier-data/Data/workshop_spatial/GIS_training/R_scripts"
@@ -219,9 +535,9 @@ r_before <- r_before*(1/0.0001)
 r_after <- r_after*(1/0.0001)
 
 
-r_nlcd30m <- raster("/nfs/bparmentier-data/Data/workshop_spatial/GIS_training/nlcd_2006_landcover_2011_edition_2014_10_10/nlcd_2006_landcover_2011_edition_2014_10_10.img")
+r_2006_nlcd30m <- raster("/nfs/bparmentier-data/Data/workshop_spatial/GIS_training/nlcd_2006_landcover_2011_edition_2014_10_10/nlcd_2006_landcover_2011_edition_2014_10_10.img")
 
-r_nlcd30m <- raster("/nfs/bparmentier-data/Data/workshop_spatial/GIS_training/nlcd_2006_landcover_2011_edition_2014_10_10/nlcd_2006.tif")
+r_nlcd_2006_30m <- raster("/nfs/bparmentier-data/Data/workshop_spatial/GIS_training/nlcd_2006_landcover_2011_edition_2014_10_10/nlcd_2006.tif")
 r_nlcd30m
 dataType(r_nlcd30m)
 
@@ -230,6 +546,18 @@ reg_sp_nlcd <- as(reg_sf_nlcd,"Spatial")
 r_nlcd30m_RITA <- crop(r_nlcd30m,reg_sp_nlcd,"test.tif",overwrite=T)
 r_nlcd30m_RITA
 
+
+aggregate_raster_fun(l_rast,
+                     zonal_colnames,
+                     use_majority,
+                     agg_fact,
+                     agg_fun,
+                     file_format,
+                     rast_ref,
+                     num_cores,
+                     out_suffix, 
+                     out_dir)
+  
 plot(r_nlcd30m_RITA)
 plot(r_nlcd30m_RITA)
 plot(r_nlcd30m_RITA==95)
