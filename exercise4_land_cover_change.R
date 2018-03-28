181b5423562fa25d160f6b1801db0cec36ff467e
Benoit Parmentier <bparmentier@sshgw02.research.sesync.org>
2018-03-28 10:45:00 -0400

    clean up of code for workshop

--- exercise4_land_cover_change.R
+++ exercise4_land_cover_change.R
@@ -11,7 +11,7 @@
 #PROJECT: SESYNC and AAG 2018 Geospatial Short Course 
 #TO DO:
 #
-#COMMIT: more processing of elevevation and roads
+#COMMIT: clean up code for workshop
 #
 #################################################################################################
 
@@ -43,8 +43,8 @@ library(readxl) #functionalities to read in excel type data
 library(psych) #pca/eigenvector decomposition functionalities
 library(sf) #spatial objects and functionalities
 library(plotrix) #various graphic functions e.g. draw.circle
-library(TOC)
-library(ROCR)
+library(TOC) # TOC and ROC for raster images
+library(ROCR) # ROCR general for data.frame
 
 ###### Functions used in this script
 
@@ -67,6 +67,8 @@ create_dir_fun <- function(outDir,out_suffix=NULL){
 in_dir_var <- "/nfs/bparmentier-data/Data/workshop_spatial/sesync2018_workshop/Exercise_4/data/"
 out_dir <- "/nfs/bparmentier-data/Data/workshop_spatial/sesync2018_workshop/Exercise_4/outputs"
 
+### General parameters
+
 #NLCD coordinate reference system: we will use this projection rather than TX.
 CRS_reg <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
 file_format <- ".tif" #raster output format 
@@ -76,6 +78,7 @@ create_out_dir_param=TRUE # if TRUE, a output dir using output suffix will be cr
 method_proj_val <- "bilinear" # method option for the reprojection and resampling 
 gdal_installed <- TRUE #if TRUE, GDAL is used to generate distance files
 
+### Input data files
 rastername_county_harris <- "harris_county_mask.tif" #Region of interest: extent of Harris County
 elevation_fname <- "srtm_Houston_area_90m.tif" #SRTM elevation
 roads_fname <- "r_roads_Harris.tif" #Road count for Harris county
@@ -175,12 +178,10 @@ levelplot(r_lc_date2,
 ## To generalize the information, let's aggregate leveraging the hierachical nature of NLCD Anderson Classification system.
 
 lc_system_nlcd_df <- read_xlsx(file.path(in_dir_var,infile_name_nlcd_classification_system))
-
 head(lc_system_nlcd_df) #inspect data
 
 ### Let's identify existing cover and compute change:
 r_stack_nlcd <- stack(r_lc_date1,r_lc_date2)
-
 freq_tb_nlcd <- as.data.frame(freq(r_stack_nlcd,merge=T))
 head(freq_tb_nlcd)
 
@@ -190,8 +191,6 @@ dim(lc_system_nlcd_df) # Now 15 land categories instead of 20.
 
 ### Selectet relevant columns for the reclassification
 rec_df <- lc_system_nlcd_df[,c(2,1)]
-
-#r_date1_rec <- subs(r_lc_date1,nlcd_legend_df[,1:2],by="id_l1","id_l2")
 r_date1_rec <- subs(r_lc_date1,rec_df,by="id_l2","id_l1")
 r_date2_rec <- subs(r_lc_date2,rec_df,by="id_l2","id_l1")
 
@@ -258,7 +257,7 @@ change_tb <- freq(r_change) #Find out how many pixels transitions to developped
 ### Distance to existing in 2001: prepare information
 r_cat2<- r_date1_rec==2 #developped in 2001
 plot(r_cat2)
-cat_bool_fname <- "developped_2001.tif"
+cat_bool_fname <- "developped_2001.tif" #input for the distance to road computation
 writeRaster(r_cat2,filename = cat_bool_fname,overwrite=T)
 
 ### Read in data for road count
@@ -266,6 +265,7 @@ r_roads <- raster(file.path(in_dir_var,roads_fname))
 plot(r_roads,colNA="black")
 res(r_roads)
 
+#### Aggregate to match the NLCD data resolution
 r_roads_90m <- aggregate(r_roads,
                          fact=3, #factor of aggregation in x and y
                          fun=mean) #function used in aggregation values
@@ -274,7 +274,7 @@ plot(r_roads_90m)
 r_roads_bool <- r_roads_90m > 0
 plot(r_roads_bool)
 
-roads_bool_fname <- "roads_bool.tif" 
+roads_bool_fname <- "roads_bool.tif" #input for the distance to road computation
 writeRaster(r_roads_bool,filename = roads_bool_fname,overwrite=T)
 
 ### This part could be transformed into a function but we keep it for clarity and learning:
@@ -282,28 +282,26 @@ if(gdal_installed==TRUE){
   
   ## Distance from developped land in 2001
   srcfile <- cat_bool_fname  
-  
   dstfile_developped <- file.path(out_dir,paste("developped_distance_",out_suffix,file_format,sep=""))
   n_values <- "1"
   
-  ### Note that gdal_proximity doesn't like when path is too long
+  ### Prepare GDAL command: note that gdal_proximity doesn't like when path is too long
   cmd_developped_str <- paste("gdal_proximity.py",
                               basename(srcfile),
                               basename(dstfile_developped),
                               "-values",n_values,sep=" ")
 
   ### Distance from roads
-  
   srcfile <- roads_bool_fname 
   dstfile_roads <- file.path(out_dir,paste("roads_bool_distance_",out_suffix,file_format,sep=""))
   n_values <- "1"
   
-  ### Note that gdal_proximity doesn't like when path is too long
+  ### Prepare GDAL command: note that gdal_proximity doesn't like when path is too long
   cmd_roads_str <- paste("gdal_proximity.py",basename(srcfile),
                          basename(dstfile_roads),
                          "-values",n_values,sep=" ")
 
-  sys_os <- as.list(Sys.info())$sysname
+  sys_os <- as.list(Sys.info())$sysname #Find what OS system is in use.
   
   if(sys_os=="Windows"){
     shell(cmd_developped_str)
@@ -326,27 +324,14 @@ plot(r_roads_distance) #This is at 90m.
 #Now rescale the distance...
 min_val <- cellStats(r_roads_distance,min) 
 max_val <- cellStats(r_roads_distance,max)
+r_roads_dist <-  (max_val - r_roads_distance) / (max_val - min_val) #high values close to 1 for areas close to roads
 
-#Linear rescaling:
-#y = ax + b with b=0
-#with 9 being new max and 0 being new min
-a = (1 - 0) /(max_val - min_val)
-#a=1
-r_roads_dist <- r_roads_distance * a
-#r_roads_dist <- (1/r_roads_distance) * 1000
 
-plot(r_roads_dist)
-
-#min_val <- cellStats(r_developped_distance,min) 
-#max_val <- cellStats(r_developped_distance,max)
-
-a = (1 - 0) /(max_val - min_val) #linear rescaling factor
-r_developped_dist <- r_developped_distance * a
-#r_developped_dist <- (1/r_developped_distance) * 1000
-#plot(r_developped_dist)
+min_val <- cellStats(r_developped_distance,min) 
+max_val <- cellStats(r_developped_distance,max)
+r_developped_dist <-  (max_val - r_developped_distance) / (max_val - min_val)
 
 ## 2) Generate var3 : slope
-
 r_elevation <- raster(file.path(in_dir_var,elevation_fname))
 
 projection(r_elevation) # This is not in the same projection as the study area. 
@@ -355,83 +340,67 @@ r_elevation_reg <- projectRaster(from= r_elevation, #input raster to reproject
                                  method= method_proj_val) #method used in the reprojection
 
 r_slope <- terrain(r_elevation_reg,unit="degrees")
-plot(r_slope)
 
 ## 3) Generate var4 : past land cover state
 
 ### reclass Land cover
-r_mask <- r_date1_rec==2
-
-#NAvalue(r_date1_rec_masked)
+r_mask <- r_date1_rec==2 #Remove developed land from  
 r_date1_rec_masked <- mask(r_date1_rec,r_mask,maskvalue=1)
-#r_date1_rec[r_date1_rec==2] <- NA
-
 plot(r_date1_rec_masked)
 
+#####################################
 ############# PART IV: Run Model and perform assessment ##############
 
-###### The logistic regression comes here:
+##############
+###### Step 1: Consistent masking and generate mask removing water (1) and developped (2) in 2001
 
-r_roads_dist
-r_developped_dist
-r_slope
-r_date1_rec_masked
-
-r_change
-
-#Generate mask removing water and developped in 2001
 #r_mask <- (r_date1_rec!=2)*(r_date1_rec!=1)*r_county_harris
 r_mask <- (r_date1_rec!=2)*(r_date1_rec!=1)
-
 plot(r_mask)
 NAvalue(r_mask) <- 0 
 
-r_county_harris <- raster(rastername_county_harris)
-
-### Sscreen for area of interest
+### Read in focus area for the modeling:
+r_county_harris <- raster(file.path(in_dir_var,rastername_county_harris))
+### Screen for area of interest
 r_mask <- r_mask * r_county_harris
 r_mask[r_mask==0]<-NA
 plot(r_mask)
-tb<- freq(r_mask)
-tb
 
-plot(r_change)
+### Check the number of NA and pixels in the study area:
+tb_study_area <- freq(r_mask)
+print(tb_study_area)
+
+## Generate dataset for Harris county
 r_variables <- stack(r_change,r_date1_rec_masked,r_slope,r_roads_dist,r_developped_dist)
-r_variables <- mask(r_variables,mask=r_mask)
-
-NAvalue(r_variables) <- -9999
-
-plot(r_variables)
-
-#plot(r_not_cat2)
+r_variables <- mask(r_variables,mask=r_mask) # mask to keep relevant area
 names(r_variables) <- c("change","land_cover","slope","roads_dist","developped_dist")
+NAvalue(r_variables) <- NA_flag_val
+
+## Examine all the variables
+plot(r_variables)
 
 ### Check for consistency in mask:
 NA_freq_tb <- freq(r_variables,value=NA,merge=T)
-?freq
-View(NA_freq_tb)
-
-### let's recombine all NA for consstencies:
+### Notice that the number of NA is not consistent.
+### Let's recombine all NA for consstencies:
 plot(r_variables)
-r_NA <- r_variables > -1
+r_NA <- r_variables > -1 #There are no negative values in the raster stack
 
-r_test <- overlay(r_NA,fun=sum)
-plot(r_test)
+r_valid_pixels <- overlay(r_NA,fun=sum)
+plot(r_valid_pixels)
 dim(r_NA)
-freq(r_test)
+freq(r_valid_pixels)
 
-r_mask <- r_test
-r_variables <- mask(r_variables,r_test)
+r_mask <- r_valid_pixels > 0
+r_variables <- mask(r_variables,r_mask)
 #r_variables <- freq(r_test2,value=NA,merge=T)
 names(r_variables) <- c("change","land_cover","slope","roads_dist","developped_dist")
 
-### May be useful to have x and y locations
-variables2_df <- (as.data.frame(r_variables))
-dim(variables2_df)
+###############
+###### Step 2: Fit glm model and generate predictions
+
 variables_df <- na.omit(as.data.frame(r_variables))
-#variables_df <- na.omit(variable_df)
 dim(variables_df)
-#View(variables_df)
 variables_df$land_cover <- as.factor(variables_df$land_cover)
 variables_df$change <- as.factor(variables_df$change)
 
@@ -440,73 +409,40 @@ names(variables_df)
 
 mod_glm <- glm(change ~ land_cover + slope + roads_dist + developped_dist, 
            data=variables_df , family=binomial())
-mod_glm
+
+print(mod_glm)
 summary(mod_glm)
-summary(mod)
+
 r_p <- predict(r_variables, mod_glm, type="response")
 plot(r_p)
-
 histogram(r_p)
-histogram(r_p,xlim=c(0,1),breaks=10)
 
-#### Do AUC to check how good it is?
-plot(subset(r_variables,"change"))
+###############
+###### Step 3: Model assessment with ROC
 
-plot(r_date1_rec_masked)
-tb_freq <- freq(r_date1_rec_masked)
-View(tb_freq)
-
-#y_var <- "change"
-#y_ref <- as.numeric(as.character(data[[y_var]])) #boolean reference values
-#index_val <- predicted_rf_mat[,2] #probabilities
-
-index_val<- mod_glm$fitted.values
-y_ref <- as.numeric(as.character(mod_glm$data$change))
-mask_val <- 1:nrow(variables_df)
-rocd2_df <- ROC(index=index_val, 
-                boolean=y_ref, 
-                mask=mask_val,
-                nthres=100)
-sum(is.na(index_val))
-sum(is.na(y_ref))
-r_p
-slot(rocd2_df,"AUC") #this is your AUC from the logistic modeling
-#Plot ROC curve:
-plot(rocd2_df,
-     main="ROC")
-
-names(rocd2_df)
-str(rocd2_df)
-
-#Access table: 
-roc_table_df <- slot(rocd2_rf,"table")
-rocd2_df
-
-### Can also use the raster directly:
+## We use the TOC package since it allows for the use of raster layers.
 
 r_change_harris <- subset(r_variables,"change")
-plot(r_change_harris)
 
-r_p_mask <- mask(r_p,r_change_harris)
-plot(r_p_mask)
-NAvalue(r_p_mask)
-rocd2_rast <- ROC(index=r_p_mask, 
-                boolean=subset(r_variables,"change"), 
-                mask=r_change_harris,
-                nthres=100)
+## These are the inputs for the assessment
+plot(r_change_harris) # boolean reference variable
+plot(r_mask) # mask for relevant observation
+plot(r_p) # index variable to assess
 
-rocd2_rast <- ROC(index=r_p, 
+roc_rast <- ROC(index=r_p, 
                   boolean=r_change_harris, 
                   mask=r_mask,
                   nthres=100)
 
-plot(rocd2_rast)
-slot(rocd2_rast,"AUC") #this is the AUC from the logistic modeling
+plot(roc_rast)
+slot(roc_rast,"AUC") #this is the AUC from ROC for the logistic modeling
 
-plot(r_p)
-freq(r_change_harris)
-plot(r_mask)
-freq(r_change_harris)
-freq(r_mask)
+toc_rast <- TOC(index=r_p, 
+                  boolean=r_change_harris, 
+                  mask=r_mask,
+                  nthres=100)
+
+plot(toc_rast)
+slot(toc_rast,"AUC") #this is the AUC from TOC for the logistic modeling
 
 ###############################  End of script  #####################################
