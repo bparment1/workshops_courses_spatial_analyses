b5cd5b743b65f2cfbc42ed526dc348fb191ed25a
Benoit Parmentier <bparmentier@sshgw02.research.sesync.org>
2018-03-31 10:45:00 -0400

    Exercise 0 updates

--- Exercise_0_intro_to_R.R
+++ Exercise_0_intro_to_R.R
@@ -1,15 +1,17 @@
 #################################### An Introduction to R #######################################
 
-#This script was distributed during the Spatial Filtering Workshop in 2008
-#Modified substantially by Benoit Parmentier on April 4, 2017 
-#for AAG2017 workshop.
+#Script by Benoit Parmentier on March 31, 2018 
+#for AAG2018 workshop and SESYNC Geospatial Data Analysis Short Course.
+#Original script from spatial filtering workshop substantially modified.
 
 ########################### PART 1: R BASICS ################
- 
+
+### We use the basic package in R rather than specialized dplyr here.
+
 ####################
 ## Numeric vectors
 
-x <- c(1,2,3) #assigning values to x (numeric vector)
+x <- c(10,11,12) #assigning values to x (numeric vector)
 x #displaying the value of x
 mean(x) #mean function works on vectors
 y <- x-mean(x)
@@ -20,12 +22,12 @@ z             # logical
 ####################
 ## Character vectors
 
-landuse <- c("urban","forest","water")
+landuse <- c("Developed high intensity","Evergreen Forest","Water")
 landuse
 landuse[1]
 lu.char <- sample(landuse,7,replace=T)
 lu.char
-lu.fact <- factor(lu.char)
+lu.fact <- factor(lu.char) # declare a factor for categorical variables
 class(lu.fact)
 lu.fact
 
@@ -39,11 +41,12 @@ mdat
 mdat %*% mdat #matrix multiplication
 mdat[1:2,]
 diag(mdat)
+image(mdat) #visualize matrix as an image
 
 ####################
 ## lists
 
-my.cats <- c("Henry","Lily","Charlie")
+my.cats <- c("Henry","Lily","Charlie") 
 age.cats <- c(5,15,4)
 my.list <- list(catname=my.cats, age=age.cats)
 my.list
@@ -57,28 +60,34 @@ my.data <- data.frame(name=my.cats,age=age.cats)
 my.data
 dim(my.data) #show the dimension of the data.frame (columns and rows)
 ls()       # you may have more objects
-my.data$age
-my.data[,2]
-my.data[,"age"]
+my.data$age #select a specific column to view data "$"
+my.data[,2] #select column 2
+my.data[,"age"] #select by name
 
 # attach/detach
-age
-attach(my.data) #not advised!! Benoit 07/15
+#age #not found
+attach(my.data) #not advised!! 
 age
 name
 detach(my.data)
-age
+#age #not found
 searchpaths()      # Search paths will be displayed
 rm(my.data)
 
 ####################
 ## read data
-in_dir <- "/Users/benoitparmentier/Dropbox/GeospatialPatternCourse/Module3/Intro_to_R/data"
+in_dir <- "/nfs/bparmentier-data/Data/workshop_spatial/sesync2018_workshop/Exercise_0/data"
 #setwd("c:\\workspace") #Windows style...
 setwd(in_dir) 
 getwd() #Get the current workind directory
 library(spdep)
-data(columbus)
+#if not installed:
+#install.packages("spdep")
+
+data(columbus) #load columbus dataset from spdep package
+dim(columbus) # size of columbus in rows x columns
+head(columbus) #view first six rows of data.frame
+
 write.table(columbus,"columbus.csv",sep=",",row.names=F)
 columbus <- read.csv("columbus.csv", header=T)
 colnames(columbus)
@@ -88,7 +97,8 @@ concord_data[1:2,c(3,8)]
 
 ####################
 ## data management
-tr <- read.dbf("turnout.dbf")
+tr <- read.dbf("turnout.dbf") #read in turnout data from Italian elections
+
 tr.sub <- tr[,c("FID_1","GDPCAP","TURNOUT01")] #subset a data.frame
 nrow(tr)
 summary(tr) #summary of the data.frame
@@ -162,10 +172,10 @@ help(rnorm)
 args(rnorm)
 example(lm)
 
-history()
-savehistory(file="IntroR.Rhistory")
+#history()
+#savehistory(file="IntroR.Rhistory")
 
-###################PART 2: Spatial R ##########
+################### PART 2: Spatial R ##########
 
 #to install a new package use:
 #install.package("package_name") eg install.package("colorRamps")
@@ -174,11 +184,12 @@ savehistory(file="IntroR.Rhistory")
 #create a raster image:
 library(raster)
 
-r <- raster()
-values(r) <- rnorm(ncell(r))
-plot(r)
+r <- raster() #create a raster image, default is latitude/longitude degrees 
+values(r) <- rnorm(ncell(r)) #assign random values to the raster
+plot(r) #quick plot of the raster image
 projection(r) #display the current projection system
-writeRaster(r,"test.tif",overwrite=TRUE)
+writeRaster(r,"test.tif",overwrite=TRUE) #save ther aster layer
+r  #check the layer
 
 #read a raster
 r1 <- raster("moore_reg_mosaiced_MOD13A2_A2005209__005_1_km_16_days_NDVI_09242013_09242013_04062014.tif")
@@ -198,26 +209,45 @@ freq(r_outliers_neg)
 ## Read in Polygon data eg ESRI shapefile
 
 #shapefile of Italy regions
-infile_shp <- "/Users/benoitparmentier/Dropbox/GeospatialPatternCourse/Module3/srm_data_v2/turnout.shp"
+in_dir <- getwd() #this should be ../data
+infile_shp <- file.path(in_dir,"turnout.shp") #joint path and name of input file
 
+library(rgdal)
 layer_path <- dirname(infile_shp) #extract path from string
-layer_name <- gsub(".shp","",basename(infile_shp)) #extract layer name
-tr <- readOGR(layer_path,layer_name)
+layer_name <- gsub(".shp","",basename(infile_shp)) #extract layer name, remove extension ".shp"
+tr_sp <- readOGR(layer_path,layer_name) # read in the shapefile
 
-spplot(tr,"TURNOUT01",col.regions=matlab.like(25),
+library(sf)
+## use sf functionality:
+tr_sf <- st_read(infile_shp) # read directly in the shapefile
+tr_sf
+
+#Plotting with sp: quick plot of vote turnout by Italian regions
+spplot(tr_sp,"TURNOUT01",col.regions=matlab.like(25),
+       main="TURNOUT") #plot turnout using specific color ramp
+### Plotting with sf:
+plot(tr_sf["TURNOUT01"],col.regions=matlab.like(25),
        main="TURNOUT") #plot turnout using specific color ramp
 
-### save figure
-out_suffix <- "wm_07152014"
+### save figure in png format
+out_suffix <- "workshop_03312018"
 png(filename=paste("Figure_turnout_Italy","_",out_suffix,".png",sep=""))
-spplot(tr,"TURNOUT01",col.regions=matlab.like(25),
-       main="TURNOUT") #plot turnout using specific color ramp
+plot(tr_sf["TURNOUT01"],col.regions=matlab.like(25),
+     main="TURNOUT") #plot turnout using specific color ramp
 dev.off()
 
 ##### 
 
-library(dismo)
+library(dismo) # you may need to install this library!!!
+#install.packages("dismo")
 mymap <- gmap("Belgium")  # choose whatever country
+plot(mymap) #google map background
+mymap <- gmap("Maryland")  # choose whatever country
+plot(mymap)
+class(mymap) #this is a raster image
+projection(mymap)
+
+mymap <- gmap("Maryland",type="satellite",lonlat = T)  # choose whatever country
 plot(mymap)
 
 ################ END OF SCRIPT ###################
