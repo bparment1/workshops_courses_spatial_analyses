51832c066c4dc7c8285d57b616d793004bb82eb3
Benoit Parmentier <bparmentier@sshgw02.research.sesync.org>
2019-03-06 14:30:00 -0500

    changes to lst plotting and naming

--- regression_temperature.py
+++ regression_temperature.py
@@ -10,7 +10,7 @@ Spyder Editor.
 #
 #AUTHORS: Benoit Parmentier
 #DATE CREATED: 09/07/2018
-#DATE MODIFIED: 03/04/2019
+#DATE MODIFIED: 03/07/2019
 #Version: 1
 #PROJECT: SESYNC Geospatial Course and AAG 2019 Python Geospatial Course
 #TO DO:
@@ -19,48 +19,138 @@ Spyder Editor.
 #
 #################################################################################################
 
+###### Library used in this script
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+import seaborn as sns
+import rasterio
+import subprocess
+import pandas as pd
+import os, glob
+from rasterio import plot
+import geopandas as gpd
+import georasters as gr
+import gdal
+import rasterio
+import descartes
+import pysal as ps
+from cartopy import crs as ccrs
+from pyproj import Proj
+from osgeo import osr
+from shapely.geometry import Point
+from collections import OrderedDict
+import webcolors
+import sklearn
 
-###### Libr  
-# Read raster bands directly to Numpy arrays.
-with rasterio.open(os.path.join(in_dir,infile)) as src:
-        r_lst = src.read(1,masked=True) #read first array with masked value, nan are assigned for NA
-        spatial_extent = rasterio.plot.plotting_extent(src)
+################ NOW FUNCTIONS  ###################
 
-#test = rasterio.open(os.path.join(in_dir,infile))
-plot.show(r_lst)
-#plot.show(r_lst,cmap='viridis',scheme='quantiles')
+##------------------
+# Functions used in the script
+##------------------
 
-src.crs # explore Coordinate Reference System 
-plot.show(src)
+def create_dir_and_check_existence(path):
+    #Create a new directory
+    try:
+        os.makedirs(path)
+    except:
+        print ("directory already exists")
+
+############################################################################
+#####  Parameters and argument set up ###########
+
+#ARGS 1
+in_dir = "/home/bparmentier/c_drive/Users/bparmentier/Data/python/climate_regression/data/Oregon_covariates"
+#ARGS 2
+out_dir = "/home/bparmentier/c_drive/Users/bparmentier/Data/python/climate_regression/outputs"
+
+#in_dir="/nfs/bparmentier-data/Data/workshop_spatial/climate_regression/data/Oregon_covariates"
+#out_dir="/nfs/bparmentier-data/Data/workshop_spatial/climate_regression/outputs"
+
+#ARGS 3:
+create_out_dir=True #create a new ouput dir if TRUE
+#ARGS 7
+out_suffix = "exercise4_03032019" #output suffix for the files and ouptut folder
+#ARGS 8
+NA_value = -9999 # NA flag balue
+file_format = ".tif"
+
+#NLCD coordinate reference system: we will use this projection rather than TX.
+CRS_reg = "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+method_proj_val = "bilinear" # method option for the reprojection and resampling
+gdal_installed = True #if TRUE, GDAL is used to generate distance files
+
+
+#epsg 2991
+crs_reg = "+proj=lcc +lat_1=43 +lat_2=45.5 +lat_0=41.75 +lon_0=-120.5 +x_0=400000 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
+
+#infile = "mean_month1_rescaled.rst" # mean LST for January
+infile = "lst_mean_month1_rescaled.tif" 
+infile_forest_perc =""
+ghcn_filename = "ghcn_or_tmax_covariates_06262012_OR83M.shp" # climate stations
+
+prop = 0.3
+random_seed= 100
+
+################# START SCRIPT ###############################
+
+######### PART 0: Set up the output dir ################
+
+#set up the working directory
+#Create output directory
+
+if create_out_dir==True:
+    #out_path<-"/data/project/layers/commons/data_workflow/output_data"
+    out_dir = "output_data_"+out_suffix
+    out_dir = os.path.join(in_dir,out_dir)
+    create_dir_and_check_existence(out_dir)
+    os.chdir(out_dir)        #set working directory
+else:
+    os.chdir(create_out_dir) #use working dir defined earlier
+
+###########################################
+### PART I: READ AND VISUALIZE DATA #######
+
+data_gpd = gpd.read_file(os.path.join(in_dir,ghcn_filename))
+
+data_gpd.head()  
+
+## Extracting information from raster using raster io object
+lst = rasterio.open(os.path.join(in_dir,infile))
+lst.crs # explore Coordinate Reference System 
+lst.shape
+lst.height
+plot.show(lst)
+
+## Read raster bands directly to Numpy arrays and visualize data
+r_lst = src.read(1,masked=True) #read first array with masked value, nan are assigned for NA
+spatial_extent = rasterio.plot.plotting_extent(src)
 type(r_lst)
 r_lst.size
-src.shape
-src.height
-
+plot.show(r_lst)
 #Can also use the regular matplotlib library function to plot images
 plt.imshow(r_lst)
-
-#see: https://matplotlib.org/users/image_tutorial.html
 plt.imshow(r_lst, clim=(259.0, 287.0))
+
+# Explore values distribution
 plt.hist(r_lst.ravel(),bins=256,range=(259.0,287.0))
+
+##### Combine raster layer and geogpanda layer
 
 data_gpd.plot(marker="*",color="green",markersize=5)
 station_or = data_gpd.to_crs({'init': 'epsg:2991'}) #reproject to  match the  raster image
 
-#https://www.earthdatascience.org/courses/earth-analytics-python/lidar-raster-data/customize-matplotlib-raster-maps/
-
-##### How to combine plots:
+##### How to combine plots with rasterio package
 fig, ax = plt.subplots()
 with rasterio.open(os.path.join(in_dir,infile)) as src:
         rasterio.plot.show((src,1),ax=ax,
                           clim=(259.0,287.0),)
-
-#plot.show(r_lst, clim=(259.0, 287.0),ax=ax)
-#with rasterio.plot.show((src,1),ax=ax)
 station_or.plot(ax=ax,marker="*",
               color="red",
                markersize=10)
-
+               
+##### How to combine plots with matplotlib package
 fig, ax = plt.subplots(figsize = (8,3))
 lst_plot = ax.imshow(r_lst, 
                        cmap='Greys', 
