a5ff2fa99f0e56cc03e2a1925949150c5990a9a6
bparment1 <benoit.parmentier@gmail.com>
2019-03-21 14:30:00 -0400

    testing sf cropping

--- exercise3_suitability_Florida.R
+++ exercise3_suitability_Florida.R
@@ -10,7 +10,7 @@
 #
 #AUTHORS: Benoit Parmentier                                             
 #DATE CREATED: 03/17/2017 
-#DATE MODIFIED: 02/26/2019
+#DATE MODIFIED: 03/21/2019
 #Version: 2
 #PROJECT: SESYNC and AAG 2019 workshop/Short Course preparation
 #TO DO:
@@ -83,7 +83,7 @@ focus_zone1_filename <- "focus_zone1.tif" #9) focus zone as raster file
 gdal_installed <- FALSE #if true use the system/shell command else use the distance layer provided
 file_format <- ".tif" #PARAM5
 NA_flag_val <- -9999 #PARAM7
-out_suffix <-"exercise3_02262019" #output suffix for the files and ouptu folder #PARAM 8
+out_suffix <-"exercise3_03212019" #output suffix for the files and ouptu folder #PARAM 8
 create_out_dir_param=TRUE #PARAM9
 
 ################# START SCRIPT ###############################
@@ -136,7 +136,7 @@ r_focus_zone1 <- raster(file.path(in_dir_var,focus_zone1_filename))
 
 ## Visualize a few datasets
 plot(r_strat_hab, main="strategic habitat")
-plot(reg_counties_sp,add=T)
+plot(reg_counties_sf$geometry,add=T)
 
 plot(r_priority_wet_hab, main="Priority wetland habitat")
 #plot(r_habitat,add=T)
@@ -164,35 +164,15 @@ clay_county_sf <- subset(reg_counties_sf,NAME=="CLAY")
 plot(r_strat_hab, main="strategic habitat")
 
 ## Crop r_strat_hab
-r_ref <- crop(r_strat_hab,clay_county_sp) #make a reference image for use in the processing
-r_ref_test <- crop(r_strat_hab,as.vector(st_bbox(clay_county_sf))[c(1, 3, 2, 4)]) #make a reference image for use in the processing
-#r_ref_test <- crop(r_strat_hab,test) #make a reference image for use in the processing
-tt <- r_ref_test - r_ref
-
-#You don't want as.vector(st_bbox(pnt_buf)) but rather as.vector(st_bbox(pnt_buf))[c(1, 3, 2, 4)], 
-#because crop expects c(xmin, xmax, ymin, ymax) – jsta Jul 15 '17 at 21:12 
-
-class(tt)
-test = extent(clay_county_sf) 
-class(test)
-
-#extent(clay_county_sf)
-#extent(clay_county_sp)
+r_ref <- crop(r_strat_hab,as.vector(st_bbox(clay_county_sf))[c(1, 3, 2, 4)]) #make a reference image for use in the processing
 
 plot(r_ref)
-#plot(clay_county_sp,border="red",add=T)
 plot(clay_county_sf$geometry,border="red",add=T)
-
-r_clay <- rasterize(clay_county_sp,r_ref) #this can be used as mask for the study area
-r_clay_test <- rasterize(clay_county_sf,r_ref) #this can be used as mask for the study area
-
+r_clay <- rasterize(clay_county_sf,r_ref) #this can be used as mask for the study area
 freq(r_clay) #check the distribution of values: 1 and NA 
-freq(r_clay_test) #check the distribution of values: 1 and NA 
-r_diff <- r_clay - r_clay_test
 
 ##Use raster of Clay county definining the study area to mask pixels
 plot(r_clay)
-
 dim(r_clay) #number of rows and columns as well as number of layers/bands
 
 ####  PART II :  HIGH BIODIVERSITY SUITABILITY LAYERS #######
@@ -230,13 +210,10 @@ plot(rc_strat_hab_reg,main="Reclassified Strategic Habit in Clay County")
 ### STEP 2: Identify Lands With High Native Biodiversity based on species count 
 
 ## Crop bio raster
-r_bio_hotspot_w <- crop(r_bio_hotspot,clay_county_sp)
 r_bio_hotspot_w <- crop(r_bio_hotspot,as.vector(st_bbox(clay_county_sf))[c(1, 3, 2, 4)])
-
 plot(r_bio_hotspot_w)
-plot(clay_county_sp,border="red",add=T)
+plot(clay_county_sf$geometry,border="red",add=T)
 
-#r_bio_clay_masked <- mask(r_bio_hotspot_w,r_clay) ## Does not work!! because resolution don't match
 #match resolution:
 projection(r_bio_hotspot_w)==projection(r_clay) #projection match
 res(r_bio_hotspot_w)==res(r_clay) #the resolutions do not match, we will need to resample
@@ -244,7 +221,6 @@ res(r_bio_hotspot_w)==res(r_clay) #the resolutions do not match, we will need to
 ## Find about resample
 #?raster::resample #to find out about the resample function from the raster package
 r_bio_hotspot_reg <- raster::resample(x=r_bio_hotspot_w,y=r_clay, method="bilinear") #Use resample to match resolutions
-
 r_bio_hotspot_reg <- mask(r_bio_hotspot_reg,r_clay) ## It now works because resolutions were matched
 plot(r_bio_hotspot_reg,main="Resampled biodiversity layer to 55m")
 
@@ -274,10 +250,11 @@ plot(rc_bio_hotspot_reg, main="Bio hotspot reclassified")
 projection(r_priority_wet_hab)
 
 ## Crop Wetland priority raster
-r_priority_wet_hab_w <- crop(r_priority_wet_hab,clay_county_sp)
+#r_priority_wet_hab_w_test <- crop(r_priority_wet_hab,clay_county_sp)
 #as.vector(st_bbox(clay_county_sf))[c(1, 3, 2, 4)]
 r_priority_wet_hab_w <- crop(r_priority_wet_hab,as.vector(st_bbox(clay_county_sf))[c(1, 3, 2, 4)])
 
+#r_diff <- r_priority_wet_hab_w_test - r_priority_wet_hab_w
 #r_priority_wet_hab_reg <- mask(r_priority_wet_hab_w,r_clay) ## Does not work!! because resolution don't match
 #match resolution:
 r_priority_wet_hab_reg <- raster::resample(r_priority_wet_hab_w,r_clay, method='bilinear') #resolution matching the study region
@@ -346,9 +323,10 @@ r_roads_bool <- writeRaster(r_roads_bool,filename=roads_bool_fname,overwrite=T)
 
 #setp 2: prepare files to create a distance to existing managed land
 
-r_flma_clay <- rasterize(flma_sp,r_clay,"OBJECTID_1",fun="max")
-r_flma_clay_test <- rasterize(flma_sf,r_clay,"OBJECTID_1",fun="max")
-test = r_flma_clay -r_flma_clay_test
+#r_flma_clay <- rasterize(flma_sp,r_clay,"OBJECTID_1",fun="max")
+#r_flma_clay_test <- rasterize(flma_sf,r_clay,"OBJECTID_1",fun="max")
+r_flma_clay <- rasterize(flma_sf,r_clay,"OBJECTID_1",fun="max")
+#test = r_flma_clay - r_flma_clay_test
 
 r_flma_clay_bool <- r_flma_clay > 0
 NAvalue(r_flma_clay_bool) <- 0 
